31c01996fa5ee416037707994996a5f0d0a9c2d4
IveanEx <yunhao.deng@kuleuven.be>
2025-09-20 00:30:20 +0200

    Bug Fix in Verilator

--- src/rtl/tc_io_cell.sv
+++ src/rtl/tc_io_cell.sv
@@ -23,9 +23,11 @@ module tc_digital_io (
     inout logic io
 );
 
+`ifndef VERILATOR
 // Pull-ups and pull-downs
 assign (weak1, weak0) io = io_pullup_en_i ? 1'b1 : 1'bz;
 assign (weak0, weak1) io = io_pulldown_en_i ? 1'b0 : 1'bz;
+`endif
 
 // IO chip -> pad
 assign io = io_direction_oe_ni ? 1'bz : data_i;
