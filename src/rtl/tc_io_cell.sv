b1a0861d93c147fc26d0de9d6c811b5f9fe641d2
Yunhao Deng <IveanEx@outlook.com>
2025-10-29 11:17:53 +0100

    Fix VCS compilation problems

--- src/rtl/tc_io_cell.sv
+++ src/rtl/tc_io_cell.sv
@@ -5,34 +5,34 @@
 // Author: Yunhao Deng <yunhao.deng@kuleuven.be>
 
 module tc_analog_io (
-    inout logic io
+    inout wire io
 );
-    assign io = 1'bz;
+  assign io = 1'bz;
 endmodule
 
 module tc_digital_io (
     // Input data
-    input logic data_i,
+    input wire data_i,
     // Output data
-    output logic data_o,
+    output wire data_o,
     // IO interface towards pad + ctrl
-    input logic io_direction_oe_ni,
-    input logic [3:0] io_driving_strength_i,
-    input logic io_pullup_en_i,
-    input logic io_pulldown_en_i,
-    inout logic io
+    input wire io_direction_oe_ni,
+    input wire [3:0] io_driving_strength_i,
+    input wire io_pullup_en_i,
+    input wire io_pulldown_en_i,
+    inout wire io
 );
 
 `ifndef VERILATOR
-// Pull-ups and pull-downs
-assign (weak1, weak0) io = io_pullup_en_i ? 1'b1 : 1'bz;
-assign (weak0, weak1) io = io_pulldown_en_i ? 1'b0 : 1'bz;
+  // Pull-ups and pull-downs
+  assign (weak1, weak0) io = io_pullup_en_i ? 1'b1 : 1'bz;
+  assign (weak0, weak1) io = io_pulldown_en_i ? 1'b0 : 1'bz;
 `endif
 
-// IO chip -> pad
-assign io = io_direction_oe_ni ? 1'bz : data_i;
+  // IO chip -> pad
+  assign io = io_direction_oe_ni ? 1'bz : data_i;
 
-// IO pad -> chip
-assign data_o = io;
+  // IO pad -> chip
+  assign data_o = io;
 
 endmodule
